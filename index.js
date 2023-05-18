7b9d63c21122d7164c3c37ee41884e4c29cfc651
siddharthadhussa <siddhartha7759@gmail.com>
2023-05-18 19:41:08 +0530

    Add existing project files to Git

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomnumber1=Math.floor(Math.random()*6)+1;
+
+var randomdiceimage="dice"+randomnumber1+".png";
+
+var randomimagesource="images/"+randomdiceimage;
+
+var image1=document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomimagesource);
+
+
+var randomnumber2=Math.floor(Math.random()*6)+1;
+
+var randomimagesource2="images/dice"+randomnumber2+".png";
+document.querySelectorAll("img")[1].setAttribute("src",randomimagesource2);
+
+if(randomnumber1>randomnumber2)
+{
+    document.querySelector("h1").innerHTML="🚩 Player 1 wins!";
+}
+else if(randomnumber1<randomnumber2)
+{
+    document.querySelector("h1").innerHTML="Player 2 wins! 🚩";
+}
+else{
+    document.querySelector("h1").innerHTML="Draw!";
+}
